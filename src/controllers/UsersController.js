a302fd65dc300eeaaed3ab5e4ff298a6eccaa75e
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-09-07 23:48:31 -0300

    adding the update method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 const sqliteConnection = require('../database/sqlite');
 const AppError = require('../utils/AppError');
 
@@ -11,7 +11,7 @@ class UsersController {
         const userAlreadyExists = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
 
         if (userAlreadyExists) {
-            throw new AppError('User already exists', 400);
+            throw new AppError('User already exists.', 400);
         }
 
         const hashedPassword = await hash(password, 8);
@@ -19,6 +19,54 @@ class UsersController {
         await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
 
         return response.status(201).json();
+    }
+
+    async update(request, response) {
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+
+        const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+        if (!user) {
+            throw new AppError('User not found.', 404);
+        }
+
+        const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError('Email already in use.', 400);
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if (password && !old_password) {
+            throw new AppError('Old password is required.', 400);
+        }
+
+        if (password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if (!checkOldPassword) {
+                throw new AppError('Old password does not match.', 400);
+            }
+
+            user.password = await hash(password, 8);
+        }
+
+        await database.run(`
+            UPDATE users SET 
+            name = ?,
+            email = ?,
+            password = ?,
+            updated_at = DATETIME('now')
+            WHERE id = ?`,
+            [user.name, user.email, user.password, id]
+        );
+
+        return response.status(200).json();
     }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,13 +4,9 @@ const UsersController = require('../controllers/UsersController');
 
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next) {
-    console.log('Middleware executado');
-    next();
-}
-
 const usersController = new UsersController();
 
-usersRoutes.post('/', myMiddleware, usersController.create);
+usersRoutes.post('/', usersController.create);
+usersRoutes.put('/:id', usersController.update);
 
 module.exports = usersRoutes;
